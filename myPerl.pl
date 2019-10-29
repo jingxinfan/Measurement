4f71b434b0e05ac071471f3435b3740827b3f01a
Fanjingxin <33148273+jingxinfan@users.noreply.github.com>
2019-10-29 13:56:44 +0800

    Add files via upload

---
+++ myPerl.pl
@@ -0,0 +1,17 @@
+﻿use Understand;
+
+$db = Understand::open("E:/MyUnderstandProject.udb");
+open(out, ">D:/results.csv");
+print out "函数名称,相对路径,CountLineCode,CountPath,Cyclomatic,MaxNesting,Knots,CountInput,CountOutput\n";
+foreach $func ($db->ents("Function ~unknown ~unresolved")) {
+	print out $func->name(), ",",
+		$func->ref("DefineIn")->file()->relname(), ",",
+		$func->metric("CountLineCode"), ",", 
+		$func->metric("CountPath"), ",",
+		$func->metric("Cyclomatic"), ",",
+		$func->metric("MaxNesting"), ",",
+		$func->metric("Knots"), ",",
+		$func->metric("CountInput"), ",",
+		$func->metric("CountOutput"), "\n"
+}
+close out
